ac76ed8a100bf13d606eb0bc39b9926663b1b6d1
skatta3 <56640542+skatta3@users.noreply.github.com>
2022-09-09 17:00:50 +0530

    Modified Files

---
+++ target/classes/DB-Table.sql
@@ -0,0 +1,9 @@
+Create Database emp;
+
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    country varchar(255),
+    PRIMARY KEY (id)
+);
